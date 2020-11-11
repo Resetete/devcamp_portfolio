7cb56915019796b5fcada0d324a4abb2d00320d9
Theresa <tmannschatz@gmail.com>
2020-11-11 15:07:11 +0100

    Implement setting data defaults for images in portfolio

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -7,4 +7,10 @@ class Portfolio < ApplicationRecord
 
   scope :html_css_items_only, -> { where(subtitle: 'HTML & CSS') }
 
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= "https://via.placeholder.com/450"
+    self.thumb_image ||= "https://via.placeholder.com/150"
+  end
 end
