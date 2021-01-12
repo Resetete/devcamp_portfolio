ea37ca667fae9051dcc44ab3edc1b6b2e4875f9d
Theresa <tmannschatz@gmail.com>
2021-01-12 16:48:36 +0100

    Update actioncable code to work with rails6

--- app/javascript/channels/blogs.js
+++ app/javascript/channels/blogs.js
@@ -1,32 +1,40 @@
-console.log('My javascript code, works?');
+import consumer from "./consumer"
 
+const App = {
+  global_chat: undefined
+}
 
 jQuery(document).on('turbolinks:load', function() {
   const comments = $('#comments');
   if (comments.length > 0) {
-    App.global_chat = App.cable.subscriptions.create({
-      channel: "BlogsChannel",
-      blog_id: comments.data('blog-id')
-    }, {
-    connected() {},
+    App.global_chat = consumer.subscriptions.create({ channel: "BlogsChannel",
+                                                      blog_id: comments.data('blog-id')
+                                                    },
+    {
+    connected() {
+      console.log('Your connected to blogs Channel');
+    },
     disconnected() {},
     received(data) {
-      return comments.append(data['comment']);
+      comments.append(data['comment']);
     },
     send_comment(comment, blog_id) {
-      return this.perform('send_comment', {comment, blog_id});
-    }
-  }
-    );
-  }
-  return $('#new_comment').submit(function(e) {
-    const $this = $(this);
-    const textarea = $this.find('#comment_content');
+      console.log('Blog_id:')
+      console.log(blog_id);
+      this.perform('send_comment', {comment, blog_id});
+   }
+  });
+ };
+
+
+  $('#new_comment').submit(function(e) {
+    let $this = $(this);
+    let textarea = $this.find('#comment_content');
     if ($.trim(textarea.val()).length > 1) {
       App.global_chat.send_comment(textarea.val(),
       comments.data('blog-id'));
       textarea.val('');
-    }
+    };
     e.preventDefault();
     return false;
   });
