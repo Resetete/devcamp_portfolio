6c7bfd5e9508aef6af07b931780d65387a8cedac
Theresa <tmannschatz@gmail.com>
2021-01-15 17:13:58 +0100

    Add status toggle radio button to blogs form

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -88,10 +88,10 @@ class BlogsController < ApplicationController
     end
 
     def blog_params
-      params.require(:blog).permit(:title, :body, :topic_id)
+      params.require(:blog).permit(:title, :body, :topic_id, :status)
     end
 
     def set_sidebar_topics
-      @set_sidebar_topics = Topic.with_blogs
+      @sidebar_topics = Topic.with_blogs
     end
 end
