30183671c911bc39a3eaebace2b23a7941d86d8f
Theresa <tmannschatz@gmail.com>
2021-01-15 16:22:50 +0100

    Add most popular topics to navbar scroller of blog

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -2,4 +2,12 @@ module BlogsHelper
   def gravatar_helper(user)
     image_tag("https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}", width: 50)
   end
+
+  def most_popular_topics_helper
+    Blog.all.group(:topic_id).count.sort_by{|key,value| value }.reverse.to_h.keys.first(6)
+  end
+
+  def topic_name_helper(topic_id)
+      Topic.where(id: topic_id).first.title
+  end
 end
