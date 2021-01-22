356dd5fb86fde57630ac71596489ef3ffd234131
Theresa <tmannschatz@gmail.com>
2021-01-22 14:32:18 +0100

    Configure the ActionCable; define the websites ActionCable can talk with; in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,8 +40,8 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = ['wss://theresa-portfolio-devcamp.herokuapp.com/cable']
+  config.action_cable.allowed_request_origins = ['https://theresa-portfolio-devcamp.herokuapp.com', 'http://theresa-portfolio-devcamp.herokuapp.com']
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
@@ -109,4 +109,6 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+
 end
